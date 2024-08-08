9c6771570d69852018a4b76bb2cbcdda503e8b50
yanivamichy <yaniv.am@YANIVAM-LT-IL.solaredge.local>
2024-08-08 12:24:43 +0300

    aaded oil nvim

--- lua/plugins/navigating.lua
+++ lua/plugins/navigating.lua
@@ -1,39 +1,72 @@
 return {
   {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-    },
-    cmd = 'Neotree',
-    keys = {
-      { '<leader>b', ':Neotree reveal<CR>', desc = 'Open File Tree' },
-    },
-    opts = {
-      default_component_configs = {
-        git_status = {
-          symbols = false,
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        keymaps = {
+          ['q'] = 'actions.close',
         },
-      },
-      filesystem = {
-        symbols = false,
-        filtered_items = {
-          visible = true,
+        win_options = {
+          signcolumn = 'yes:2',
         },
-        window = {
-          mappings = {
-            ['<leader>b'] = 'close_window',
-          },
+        columns = {
+          -- 'permissions',
+          -- 'size',
+          -- 'mtime',
         },
-      },
-    },
+        view_options = { show_hidden = true },
+      }
+      vim.keymap.set('n', '-', ':Oil --float<CR>', { desc = 'Open parent directory' })
+      vim.keymap.set('n', '<leader>b', ':Oil --float<CR>', { desc = 'Open parent directory' })
+    end,
   },
 
   {
+    'refractalize/oil-git-status.nvim',
+    dependencies = { 'stevearc/oil.nvim' },
+    config = true,
+  },
+
+  --   version = '*',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+  --     'MunifTanjim/nui.nvim',
+  --   },
+  --   cmd = 'Neotree',
+  --   keys = {
+  --     { '<leader>b', ':Neotree reveal<CR>', desc = 'Open File Tree' },
+  --   },
+  --   opts = {
+  --     default_component_configs = {
+  --       git_status = {
+  --         symbols = false,
+  --       },
+  --     },
+  --     filesystem = {
+  --       symbols = false,
+  --       filtered_items = {
+  --         visible = true,
+  --       },
+  --       window = {
+  --         mappings = {
+  --           ['<leader>b'] = 'close_window',
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
+
+  {
     'AckslD/messages.nvim',
-    config = 'require("messages").setup()',
+    config = function()
+      require('messages').setup()
+      Msg = function(...)
+        require('messages.api').capture_thing(...)
+      end
+    end,
     keys = {
       {
         '<leader>m',

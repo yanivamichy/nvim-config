6d8c81f8bd2f3c9a282b84df6f0ae228bbbeb482
yanivamichy <yaniv@YanivPC>
2024-08-10 22:03:06 +0300

    set debug configuration for python

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -62,7 +62,38 @@ return {
       dap.listeners.before.event_terminated['dapui_config'] = dapui.close
       dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-      require('dap-python').setup '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
+      -- require('dap-python').setup '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
+      dap.adapters.python = {
+        type = 'executable',
+        command = vim.fn.expand '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python',
+        args = { '-m', 'debugpy.adapter' },
+        options = {
+          source_filetype = 'python',
+        },
+      }
+      dap.configurations.python = {
+        {
+          type = 'python',
+          request = 'launch',
+          name = 'Launch File',
+          console = 'integratedTerminal',
+          program = '${file}',
+          cwd = '${workspaceFolder}',
+          python = function()
+            local cwd = vim.fn.getcwd()
+            if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+              return cwd .. '/venv/bin/python'
+            elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+              return cwd .. '/.venv/bin/python'
+            else
+              return '/usr/bin/python3'
+            end
+          end,
+          justMyCode = false,
+          stopOnEntry = false,
+          env = { PYTHONPATH = '${workspaceFolder}' .. ':' .. (os.getenv 'PYTHONPATH' or '') },
+        },
+      }
     end,
   },
 }

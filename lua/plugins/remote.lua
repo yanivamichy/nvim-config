107aac50a9bd0e4a826a4469233ea43d34e9825a
yanivamichy <yaniv.am@YANIVAM-LT-IL.solaredge.local>
2024-08-21 17:07:47 +0300

    bla

--- lua/plugins/remote.lua
+++ lua/plugins/remote.lua
@@ -2,16 +2,19 @@ return {
   -- {
   --   'chipsenkbeil/distant.nvim',
   --   branch = 'v0.3',
+  --   -- version = '0.1.2',
   --   config = function()
   --     require('distant'):setup()
   --   end,
   -- },
-  'amitds1997/remote-nvim.nvim',
-  version = 'v0.3.11', -- Pin to GitHub releases
-  dependencies = {
-    'nvim-lua/plenary.nvim', -- For standard functions
-    'MunifTanjim/nui.nvim', -- To build the plugin UI
-    'nvim-telescope/telescope.nvim', -- For picking b/w different remote methods
+  {
+    'amitds1997/remote-nvim.nvim',
+    version = 'v0.3.11', -- Pin to GitHub releases
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- For standard functions
+      'MunifTanjim/nui.nvim', -- To build the plugin UI
+      'nvim-telescope/telescope.nvim', -- For picking b/w different remote methods
+    },
+    config = true,
   },
-  config = true,
 }

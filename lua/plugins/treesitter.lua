b7a21837e06530ccfde679f1fddb567e6592c962
yanivamichy <yaniv.am@YANIVAM-LT-IL.solaredge.local>
2024-11-25 17:45:13 +0200

    treesitter context - single line per context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -36,7 +36,9 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
-      require('treesitter-context').setup {}
+      require('treesitter-context').setup {
+        multiline_threshold = 1,
+      }
       vim.keymap.set('n', '[c', function()
         require('treesitter-context').go_to_context(vim.v.count1)
       end, { silent = true })
--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -45,15 +45,15 @@ return {
       -- Document existing key chains
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
         ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
         ['<leader>gs'] = { name = '[G]it [S]earch', _ = 'which_key_ignore' },
-        ['<leader>gu'] = { name = '[U]nit tests', _ = 'which_key_ignore' },
+        ['<leader>u'] = { name = '[U]nit tests', _ = 'which_key_ignore' },
         ['<leader>l'] = { name = '[L]sp symbols', _ = 'which_key_ignore' },
       }
     end,

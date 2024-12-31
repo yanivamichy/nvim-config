730bcf952b660455f9c0005b571efa86529dfe17
yanivamichy <yaniv.am@YANIVAM-LT-IL.solaredge.local>
2024-12-31 17:07:55 +0200

    changed yank registers

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -30,7 +30,7 @@ return {
     },
     init = function()
       vim.g.db_ui_use_nerd_fonts = vim.g.have_nerd_font
-      require('cmp').setup.filetype({ 'mysql' }, {
+      require('cmp').setup.filetype({ 'mysql', 'sql' }, {
         sources = {
           { name = 'vim-dadbod-completion' },
           { name = 'buffer' },

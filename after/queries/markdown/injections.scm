c80d05ae9c0551b880986c7d015a95a319b80831
yaniv <yaniv@YanivComp.localdomain>
2026-05-11 11:33:00 +0300

    tikz injection as latex

--- after/queries/markdown/injections.scm
+++ after/queries/markdown/injections.scm
@@ -7,3 +7,11 @@
 	(#match? @lang "^\\.?(matplotlib|pyplt)$")
 	(#set! injection.language "python")
 )
+
+(
+	fenced_code_block
+	(info_string (language) @lang)
+	(code_fence_content) @injection.content
+	(#match? @lang "^\\.?tikz$")
+	(#set! injection.language "latex")
+)
